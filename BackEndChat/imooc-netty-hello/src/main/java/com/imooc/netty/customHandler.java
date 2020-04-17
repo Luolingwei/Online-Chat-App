1e353314b71f40aaeca8f60d89aad1846cc122d5
LingweiLuo <564258080@qq.com>
2020-04-16 23:26:42 -0400

    update

---
+++ BackEndChat/imooc-netty-hello/src/main/java/com/imooc/netty/customHandler.java
@@ -0,0 +1,106 @@
+package com.imooc.netty;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+
+/**
+ * 创建自定义助手类
+ */
+
+// SimpleChannelInboundHandler: 对于请求来讲，相当于[入站，入境]
+public class customHandler extends SimpleChannelInboundHandler<HttpObject> {
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel注册~");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel移除~");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel活跃~");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel不活跃~");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel读取完毕~");
+        super.channelReadComplete(ctx);
+    }
+
+    @Override
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+        System.out.println("channel用户事件触发~");
+        super.userEventTriggered(ctx, evt);
+    }
+
+    @Override
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel可写更改~");
+        super.channelWritabilityChanged(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        System.out.println("捕获到异常~");
+        super.exceptionCaught(ctx, cause);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("助手类添加~");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("助手类移除~");
+        super.handlerRemoved(ctx);
+    }
+
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg)
+            throws Exception {
+
+        // 获取channel
+        Channel channel = ctx.channel();
+
+        // 显示客户端的远程地址
+        if (msg instanceof HttpRequest) {
+            System.out.println(channel.remoteAddress());
+
+            // 定义发送的数据消息
+            ByteBuf content = Unpooled.copiedBuffer("Hello netty~", CharsetUtil.UTF_8);
+
+            // 构建一个Http response
+            FullHttpResponse response = new DefaultFullHttpResponse
+                    (HttpVersion.HTTP_1_1,
+                            HttpResponseStatus.OK,
+                            content);
+
+            // 为响应增加数据类型和长度
+            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
+            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+
+            // 把响应刷到客户端
+            ctx.writeAndFlush(response);
+        }
+
+    }
+}
